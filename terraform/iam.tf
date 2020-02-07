a12241cbef616389886ddad92e1349abee70c56b
Rosie Amphlett <rosie.amphlett@thoughtworks.com>
2020-02-07 17:02:28 +0000

    [PRMT-942] Update iam.tf to be consistent with component template

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -23,17 +23,38 @@ resource "aws_iam_role" "administration-portal-ecs-role" {
 }
 
 
-data "aws_iam_policy_document" "ssm_policy_doc" {
+data "aws_iam_policy_document" "ecr_policy_doc" {
   statement {
     actions = [
-      "secretsmanager:GetResourcePolicy",
-      "secretsmanager:GetSecretValue",
-      "secretsmanager:DescribeSecret",
-      "secretsmanager:ListSecretVersionIds"
+      "ecr:*"
     ]
 
     resources = [
-      "arn:aws:secretsmanager:${var.region}:${local.account_id}:secret:/NHS/${var.environment}-${local.account_id}/${var.component_name}/authorization_keys"
+      "*"
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "logs_policy_doc" {
+  statement {
+    actions = [
+      "logs:*"
+    ]
+
+    resources = [
+      "*"
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "ssm_policy_doc" {
+  statement {
+    actions = [
+      "ssm:*"
+    ]
+
+    resources = [
+      "arn:aws:ssm:${var.region}:${local.account_id}:parameter/NHS/${var.environment}-${local.account_id}/${var.component_name}/authorization_keys"
     ]
   }
 }
@@ -43,7 +64,27 @@ resource "aws_iam_policy" "ssm_policy" {
   policy = data.aws_iam_policy_document.ssm_policy_doc.json
 }
 
-resource "aws_iam_role_policy_attachment" "ssm_police_attach" {
+resource "aws_iam_policy" "ecr_policy" {
+  name   = "${var.environment}-${var.component_name}-ecr"
+  policy = data.aws_iam_policy_document.ecr_policy_doc.json
+}
+
+resource "aws_iam_policy" "logs_policy" {
+  name   = "${var.environment}-${var.component_name}-logs"
+  policy = data.aws_iam_policy_document.logs_policy_doc.json
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_policy_attach" {
   role       = aws_iam_role.administration-portal-ecs-role.name
   policy_arn = aws_iam_policy.ssm_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_policy_attach" {
+  role       = aws_iam_role.administration-portal-ecs-role.name
+  policy_arn = aws_iam_policy.ecr_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "logs_policy_attach" {
+  role       = aws_iam_role.administration-portal-ecs-role.name
+  policy_arn = aws_iam_policy.logs_policy.arn
 }
