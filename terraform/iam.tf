68c04a8e62b209e1e019f8abb605bbd0b612bd93
Helen Zhou <helen.zhou@thoughtworks.com>
2020-02-07 15:22:24 +0000

    [PRMT-942] Update iam.tf terraform to link up authorisation keys

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,3 +1,7 @@
+locals {
+  account_id = data.aws_caller_identity.current.account_id
+}
+
 data "aws_iam_policy_document" "ecs-assume-role-policy" {
   statement {
     actions = ["sts:AssumeRole"]
@@ -8,6 +12,7 @@ data "aws_iam_policy_document" "ecs-assume-role-policy" {
         "ecs-tasks.amazonaws.com"
       ]
     }
+    
   }
 }
 
@@ -15,4 +20,30 @@ resource "aws_iam_role" "administration-portal-ecs-role" {
   name               = "${var.environment}-${var.component_name}-EcsTaskRole"
   assume_role_policy = data.aws_iam_policy_document.ecs-assume-role-policy.json
   description        = "Role assumed by ${var.component_name} ECS task"
+}
+
+
+data "aws_iam_policy_document" "ssm_policy_doc" {
+  statement {
+    actions = [
+      "secretsmanager:GetResourcePolicy",
+      "secretsmanager:GetSecretValue",
+      "secretsmanager:DescribeSecret",
+      "secretsmanager:ListSecretVersionIds"
+    ]
+
+    resources = [
+            "arn:aws:secretsmanager:${var.region}:${local.account_id}:secret:/NHS/${var.environment}-${local.account_id}/${var.component_name}/authorization_keys"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "ssm_policy" {
+  name   = "${var.environment}-${var.component_name}-ssm"
+  policy = data.aws_iam_policy_document.ssm_policy_doc.json
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_police_attach" {
+  role       = aws_iam_role.component-ecs-role.name
+  policy_arn = aws_iam_policy.ssm_policy.arn
 }
