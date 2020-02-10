9de108dd0b648b365980a2965a03735c2095c750
Helen Zhou <helen.zhou@thoughtworks.com>
2020-02-10 12:27:33 +0000

    [PRMT-942] Add GetAuthorizationToken to Task IAM Role - based on commit in component-template repo

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -32,6 +32,15 @@ data "aws_iam_policy_document" "ecr_policy_doc" {
     resources = [
       "arn:aws:ecr:${var.region}:${local.account_id}:repository/deductions/${var.component_name}"
     ]
+  statement {
+    actions = [
+      "ecr:GetAuthorizationToken"
+    ]
+
+    resources = [
+            "*"
+    ] 
+  }
   }
 }
 
