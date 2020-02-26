ae6ed88590216d459038d1dd2a76ceca9b8c3d32
Phil Evans <phil.evans@thoughtworks.com>
2020-02-26 11:43:34 +0000

    [PRMT-1013] Move https alb listener to deductions-infra

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -18,7 +18,7 @@ resource "aws_alb_target_group" "alb-tg" {
 
 resource "aws_alb_listener_rule" "alb-http-listener-rule" {
   listener_arn = data.aws_ssm_parameter.deductions_private_alb_httpl_arn.value
-  priority     = 209
+  priority     = 301
 
   action {
     type = "redirect"
@@ -38,7 +38,7 @@ resource "aws_alb_listener_rule" "alb-http-listener-rule" {
 
 resource "aws_alb_listener_rule" "alb-https-listener-rule" {
   listener_arn = data.aws_ssm_parameter.deductions_private_alb_httpsl_arn.value
-  priority     = 101
+  priority     = 302
 
   action {
     type             = "forward"
