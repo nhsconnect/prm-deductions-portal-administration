347fb17a93e392320a301f77898af4190248f873
Helen Zhou <helen.zhou@thoughtworks.com>
2020-03-13 09:40:00 +0000

    Rename pipeline files, and adjust smoke test (to object containing)

--- server/smoke.test.js
+++ server/smoke.test.js
@@ -1,12 +1,12 @@
-import config from './config';
 import axios from 'axios';
 import { message } from './api/health';
+import config from './config';
 
 describe('Smoke test', () => {
   it('health endpoint returns matching data', async () => {
     const healthUrl = `${config.url}/health`;
     const res = await axios.get(healthUrl);
 
-    expect(res.data).toEqual(message);
+    expect(res.data).toEqual(expect.objectContaining(message));
   });
 });
